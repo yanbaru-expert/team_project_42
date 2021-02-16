206ecec967339e6fbf17b066884e4dcba1c9d5a6
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2021-02-16 20:47:55 +0900

    差分を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# textsテーブルのデータを全削除し、'db/csv_data/text_data.csv'のデータを投入
+ImportCsv.text_data
+
+# moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入
+ImportCsv.movie_data
+
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
