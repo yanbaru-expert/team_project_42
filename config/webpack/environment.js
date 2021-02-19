ce2304103d118ca59a89dba5ae3fbaf165f128ce
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2021-02-19 16:37:39 +0900

    Rails6の場合のBootstrapの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+// ***** 以下を追加 *****
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+}))
+// ***** 以上を追加 *****
+
 module.exports = environment
