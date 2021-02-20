91b21f6d6ccc41378fb9ee27bfb0cb13bcecdf7c
Hiroto-Iizuka <oroxith@gmail.com>
2021-02-21 08:47:53 +0900

    管理者画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  root "texts#index"
-  resources :texts
+  devise_for :admin_users, path: "admin"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,10 @@ ImportCsv.text_data
 
 # moviesテーブルのデータを全削除し、'db/csv_data/movie_data.csv'のデータを投入
 ImportCsv.movie_data
-AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+ADMIN_EMAIL = "admin@example.com"
+PASSWORD = "password"
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
+  admin_user.password = PASSWORD
+  puts "管理者ユーザーの初期データインポートに成功しました。"
+end
