1aa5c316c924e2a60993ec7fd5f505bd6e0aba25
Hiroto-Iizuka <oroxith@gmail.com>
2021-02-25 21:11:55 +0900

    seeds.rbの管理者インポートを何度も実装可能に

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,4 +1,8 @@
 ActiveAdmin.setup do |config|
+  # == Site Title
+  #
+  # Set the title that is displayed on the main layout
+  # for each of the active admin pages.
   #
   config.site_title = "Team Project"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,4 +22,7 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts "ユーザーの初期データインポートに成功しました。"
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.find_or_create_by!(email: 'admin@example.com') do |admin_user|
+  user.password = PASSWORD
+  puts "管理者の初期データインポートに成功しました。"
+end
