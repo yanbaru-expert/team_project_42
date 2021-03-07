1bb1a7cb098fb13d39b89979bd617813dc70450a
Stoyama <Stoyama@MacBook.local>
2021-03-07 14:25:43 +0900

    読破済機能の修正

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,9 @@
 class TextsController < ApplicationController
   def index
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"])
-    @read_text_ids = current_user.reads.pluck(:text_id)
   end
 
   def show
     @text = Text.find(params[:id])
-    @read_text_ids = current_user.reads.pluck(:text_id)
   end
 end
