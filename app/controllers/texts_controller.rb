be3db5915c5d08553d886a681addba3cb3cafef1
satou54 <satou54satou@gmail.com>
2021-03-09 20:13:54 +0900

    text_controller.rbのindexアクションをリファクタリング #55

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,22 +1,29 @@
 class TextsController < ApplicationController
+  before_action :set_texts, only: :index
+
   def index
-    genre = params[:genre]
-
-    case genre
-    when "php"
-      genre = "Php"
-      @title = "PHP"
-    when "aws"
-      genre = @title = "AWS"
-    when nil
-      genre = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
-      @title = "Ruby/Rails"
-    end
-
-    @texts = Text.where(genre: genre)
+    @texts = Text.where(genre: @search_genre)
   end
 
   def show
     @text = Text.find(params[:id])
+  end
+
+  private
+
+  def set_texts
+    genre = params[:genre]
+
+    @search_genre = case genre
+      when "php"
+        @title = "PHP"
+        "Php"
+      when "aws"
+        @title = "AWS"
+        "AWS"
+      when nil
+        @title = "Ruby/Rails"
+        ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+      end
   end
 end
