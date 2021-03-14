670996a67ae7c42e8062da8e843a17e5b89ba78a
satou54 <satou54satou@gmail.com>
2021-03-15 01:55:04 +0900

    検索機能をリファクタリング #38

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,8 +1,7 @@
 class TextsController < ApplicationController
-  before_action :set_texts, only: :index
-  before_action :set_q, only: :index
-
   def index
+    @q = set_texts.ransack(params[:q])
+    @texts = @q.result
   end
 
   def show
@@ -12,12 +11,10 @@ class TextsController < ApplicationController
   private
 
   def set_texts
-    @genre = params[:genre]
-    search_flg = false
-
     if params[:q]
-      search_flg = true
       @genre = params[:q]["genre"]
+    else
+      @genre = params[:genre]
     end
 
     search_genre = case @genre
@@ -35,17 +32,6 @@ class TextsController < ApplicationController
         ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
       end
 
-    if search_flg
-      search_genre_text = Text.where(genre: search_genre)
-      @q = search_genre_text.ransack(params[:q])
-      @texts = @q.result
-      search_flg = false
-    else
-      @texts = Text.where(genre: search_genre)
-    end
-  end
-
-  def set_q
-    @q = Text.ransack(params[:q])
+    @texts = Text.where(genre: search_genre)
   end
 end
